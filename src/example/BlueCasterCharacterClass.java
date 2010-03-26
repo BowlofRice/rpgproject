c4610f557c68a869d89ed21e77f067b67de78583
aesca <aesca.student@manhattan.edu>
2010-03-26 16:58:22 -0400

    Added a number of character classes, one for each of the pieces you can choose and some arbitrary values which we can alter and what not when debugging.

---
+++ src/example/BlueCasterCharacterClass.java
@@ -0,0 +1,35 @@
+package example;
+
+/**
+ *
+ * @author andrew
+ */
+public class BlueCasterCharacterClass {
+
+    int level = 1;
+    int location = 1;//default starting location
+    int attack = 8;
+    int upgradelevelmeter = 0;
+    int range = 2;
+
+    void gainexperience(int upgradelevelmeter)
+    {
+        upgradelevelmeter += .05;
+        if(upgradelevelmeter >= 1 && level<=3)
+        {
+            increaselevel();
+            upgradelevelmeter = 0;
+        }
+    }
+
+    void increaselevel(){
+        increase_attack();
+        level++;
+    }
+
+    void increase_attack(){
+        attack=attack*2;
+    }
+
+
+}
