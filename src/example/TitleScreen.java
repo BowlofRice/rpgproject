cf81812a216593bd4c317118afc387c9650f027c
Andres Ramirez <aramirez01.student@manhattan.edu>
2010-04-06 22:25:26 -0400

    re did the menus. still using one canvas to do all rendering. figured out problem navigating menus. it had to do with using the same key for multiple screens. ie: using enter as the catalyst to go to another page. fixed it by using different keys in different screens, as noted in the screen currently viewed. this makes more sense if its actually run.

--- src/example/TitleScreen.java
+++ src/example/TitleScreen.java
@@ -1,34 +1,27 @@
 package example;
 
-import java.awt.Button;
-import java.awt.Color;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class TitleScreen{
+public class TitleScreen {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JFrame rpgproto=new JFrame("RPG Prototype");
-		Field gamearea=new Field(rpgproto);
-		gamearea.setBackground(Color.BLACK);
-		rpgproto.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		rpgproto.addKeyListener(new KeyListen());
-		rpgproto.addMouseListener(new MouseListen());
-		rpgproto.setResizable(false);
-		rpgproto.add(gamearea);
-		rpgproto.pack();
-		rpgproto.setLocationRelativeTo(null);
-		rpgproto.setVisible(true);
+		// TODO Auto-generated method stub
+		JFrame sample=new JFrame("Sample");
+		sample.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sample.setResizable(false);
+		sample.addKeyListener(new KeyListen());
+		Field dS=new Field();
+		sample.add(dS);
+		sample.pack();
+		sample.setLocationRelativeTo(null);
+		sample.setVisible(true);
 		while(true){
-			gamearea.repaint();
-			gamearea.timestep();
+			dS.timestep();
+			dS.repaint();
 		}
 	}
-
-
 
 }
