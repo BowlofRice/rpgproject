7233d9ec13222707859ec4796da8d05fbd745565
aesca <aesca.student@manhattan.edu>
2010-04-17 12:20:03 -0400

    ..

--- src/example/MapAltTwo.java
+++ src/example/MapAltTwo.java
@@ -152,30 +152,30 @@ public class MapAltTwo {
         Image img0 = Toolkit.getDefaultToolkit().getImage("../rpgproject/src/example/tiles/char1.gif");
         double randomnum = 0;
         int rand = 0;
-        while (Matrix[i][j] > 0 && j < 19 && okaydevice ==0 ) {
+        while (Matrix[i][j] > 0 && j < 19 && okaydevice == 0 ) {
             randomnum = Math.random();
             randomnum = randomnum * 6;
             rand = (int) randomnum;
-            if (rand < 2 && Matrix[i][j] < Matrix[i][j + 1]) {
+            if (rand < 2 && Matrix[i][j] <= Matrix[i][j + 1]) {
                 g.drawImage(img1, i * Matrix.length, j * Matrix[0].length, null/*(ImageObserver) this*/);
                 g.drawImage(img0, i * Matrix.length, (j + 1) * Matrix[0].length, null);
                 //Matrix[i][j] = Matrix[i][j+1];
                 okaydevice=1;
 
                 j += 1;
-            } else if (2 <= rand && rand <= 3 && Matrix[i][j] < Matrix[i][j - 1]) {
+            } else if (2 <= rand && rand <= 3 && Matrix[i][j] <= Matrix[i][j - 1]) {
                 g.drawImage(img1, i * Matrix.length, j * Matrix[0].length, null/*(ImageObserver) this*/);
                 g.drawImage(img0, i * Matrix.length, (j - 1) * Matrix[0].length, null);
                 //Matrix[i][j] = Matrix[i][j-1];
                 okaydevice=1;
                 j -= 1;
-            } else if (3 < rand && rand <= 4 && Matrix[i][j] < Matrix[i + 1][j]) {
+            } else if (3 < rand && rand <= 4 && Matrix[i][j] <= Matrix[i + 1][j]) {
                 g.drawImage(img1, i * Matrix.length, j * Matrix[0].length, null/*(ImageObserver) this*/);
                 g.drawImage(img0, (i + 1) * Matrix.length, j * Matrix[0].length, null);
                 //Matrix[i][j] = Matrix[i+1][j];
                 okaydevice=1;
                 i += 1;
-            } else if (4 < rand && rand <= 6 && Matrix[i][j] < Matrix[i - 1][j]) {
+            } else if (4 < rand && rand <= 6 && Matrix[i][j] <= Matrix[i - 1][j]) {
                 g.drawImage(img1, i * Matrix.length, j * Matrix[0].length, null/*(ImageObserver) this*/);
                 g.drawImage(img0, (i - 1) * Matrix.length, j * Matrix[0].length, null);
                 //Matrix[i][j] = Matrix[i-1][j];
