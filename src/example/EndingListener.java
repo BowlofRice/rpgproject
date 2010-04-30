ff0a98762e1a394caf102ac3e774bf670caee351
aesca <aesca.student@manhattan.edu>
2010-04-29 22:57:58 -0400

    cc

---
+++ src/example/EndingListener.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package example;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author andrew
+ */
+public class EndingListener implements ActionListener{
+
+    public void actionPerformed(ActionEvent e) {
+
+        //exits for now
+        System.exit(0);
+    }
+
+
+
+}
