353502678bf8e9fe4b70d457fa7c4e4caa952071
aesca <aesca.student@manhattan.edu>
2010-05-02 19:56:36 -0400

    ..

--- src/example/CharUpgrade.java
+++ src/example/CharUpgrade.java
@@ -11,10 +11,23 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.Array;
+import javax.swing.JCheckBox;
 
-public class CharUpgrade extends JFrame{
+public class CharUpgrade extends JFrame {
 
     JFrame charUpgrade = new JFrame();
+    
+    //??
+    public int num_allies = MouseTracer.allies.size();
+
+    public String[] checkboxes = new String[num_allies];
+    public String holder;
+
+
+
 
 
 
@@ -23,14 +36,62 @@ public class CharUpgrade extends JFrame{
     public CharUpgrade(){
         
         super();
-        JPanel myPanel = new JPanel();
+        setLayout(new BorderLayout());
+
+        JPanel myCheckBoxPanel = new JPanel();
+        myCheckBoxPanel.setLayout(new GridLayout(0,1));
+        add(myCheckBoxPanel,BorderLayout.WEST);
+
+        for(int i=0; i<num_allies;i++)
+        {
+            //attempting to set the value in the string array of i to
+            //word CheckBoxi
+            checkboxes[i] = ("CheckBox"+i);
+            holder = checkboxes[i];
+
+            //attempting to use that same string just created as name for
+            //checkbox to be added
+
+            myCheckBoxPanel.add(new JCheckBox(holder));
+
+
+        }
+
+
+        JPanel myGoodGuysToUpgrade = new JPanel();
+        myGoodGuysToUpgrade.setLayout(new GridLayout(0,1));
+        add(myGoodGuysToUpgrade,BorderLayout.CENTER);
+
+        JLabel typeOfGoodGuy = new JLabel();
+
+        for(int i=0;i<num_allies;i++)
+        {
+            //trying to set holder to contain the type of ally at that point in the vector
+            holder = MouseTracer.allies.get(i);
+
+            //setting the label to have holder as the label
+            typeOfGoodGuy.setText(holder);
+
+            //adding label to panel
+            myGoodGuysToUpgrade.add(typeOfGoodGuy);
+        }
+
+        JLabel title = new JLabel("Check Units to Upgrade");
+        add(title,BorderLayout.NORTH);
+
+
+
+
+        //adds upgrade level button which has action listener, which uses
+        //class EndingListener, which currently just closes program
+
         JButton upgradeGo = new JButton("Upgrade Level");
         EndingListener buttonEar = new EndingListener();
         upgradeGo.addActionListener(buttonEar);
         charUpgrade.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 
-        myPanel.add(upgradeGo);
+        add(upgradeGo,BorderLayout.SOUTH);
         
 
     }
